fa6dedfbd230d8a2f17118c1f1ded2cb5f843982
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-27 15:59:10 +0530

    class Component Creating Test

---
+++ src/HomePage/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+class HomePage extends Component{
+
+
+    state = {
+
+
+    }
+
+    render () {
+
+        return(
+            <View>Hi</View>
+
+        );
+    }
+}
+
+export default HomePage;
