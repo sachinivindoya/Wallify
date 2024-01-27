7bfbc749f32ef092f75ea661b28e9f3728f0f9a1
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-01-27 19:52:56 +0530

    Favourite Tab Testing

---
+++ src/HomePage/Favourite/index.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class FavoritesTab extends Component{
+    state = {}
+
+    render () {
+         return (
+             <Text> Favorites Tab </Text>
+          );
+   }
+    
+}
+export default FavoritesTab
